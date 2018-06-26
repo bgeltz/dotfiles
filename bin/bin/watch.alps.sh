ab162247b070d2fb0c301a1441ab132128eafa33
Brad Geltz <brad.geltz@intel.com>
2018-06-26 13:17:36 -0700

    Moved byobu to by.  Added watch.alps.sh
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

---
+++ bin/bin/watch.alps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ; echo ; ls -lta ${1}"
+#watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ; echo ; tail -n 20 ${1}"
+watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ;"
+
