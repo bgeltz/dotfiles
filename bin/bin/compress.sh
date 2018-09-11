8c5fe05fa6dca0bfabbe7f2430da7f207b50bd5f
Brad Geltz <brad.geltz@intel.com>
2018-09-11 11:55:48 -0700

    Misc updates.
    - compress.sh - Use basename insteead of bash string expansion.
    - go1 - Cleanup; remove VERSION file on clean build.
    - watch.sh - Update format string, simpify watch expression.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/compress.sh
+++ bin/bin/compress.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-tar cf - ${1} | pigz -9 > ${1%/}.tgz
-# tar -cvz --use-compress-program=pigz -f ${1/%/} ${1} 
+tar cf - ${1} | pigz -9 > $(basename ${1}).tgz
+# tar -cvz --use-compress-program=pigz -f ${1/%/} ${1}
+# tar cf - ${1} | pigz -9 > ${1%/}.tgz
