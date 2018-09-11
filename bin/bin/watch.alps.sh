233ad58c1b4739608c629a1084715074063fd9e8
Brad Geltz <brad.geltz@intel.com>
2018-09-11 12:01:23 -0700

    Refactor watch.alps.sh
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/watch.alps.sh
+++ bin/bin/watch.alps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+QUEUE=Q.GEO_test
+# QUEUE=R.GEO_res1
+
 #watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ; echo ; ls -lta ${1}"
-#watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ; echo ; tail -n 20 ${1}"
-watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) default; echo ; qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue Q.GEO_test ;"
+watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ;"
+#watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami); echo ; qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue ${QUEUE} ;"
 
