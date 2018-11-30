7afbc2a247f7337b453b23412f308a86fb3d4ad9
Brad Geltz <brad.geltz@intel.com>
2018-11-30 13:51:23 -0800

    WIP: Script to install py packages.
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/install_packages.sh
+++ bin/bin/install_packages.sh
@@ -18,14 +18,29 @@ else
     # python get-pip.py --user
 fi
 
+if [ ! -z ${DARSHAN_PRELOAD} ]; then
+    echo "Error: Darshan was detected in your environment and is known to conflict with GEOPM."
+    echo "Run \"module unload darshan\"."
+    exit 1
+fi
+
 TMP_DIR=${HOME}/tmp/pydeps
+echo "Removing ${TMP_DIR}..."
+rm -fr ${TMP_DIR}
 
 virtualenv ${TMP_DIR}
 source ${TMP_DIR}/bin/activate
 
-pip install pandas natsort matplotlib cycler tables psutil numexpr bottleneck
+# MIC_FLAG='-xCORE-AVX2'
+# export CC="cc ${MIC_FLAG} -dynamic"
+# export CXX="CC ${MIC_FLAG} -dynamic"
+# The only way I've gotten psutil to load is with:
+#   module swap PrgEnv-intel PrgEnv-gnu
+#   module unload darshan
+#   Then run this.
+
+pip install --ignore-installed --no-cache-dir pandas natsort matplotlib cycler tables psutil numexpr bottleneck
 
 deactivate
 
 echo "Deps installed into ${TMP_DIR}/lib/python2.7/site-packages"
-
