40fc0363b49c07e3b02658cfa26636bffa3770a6
Brad Geltz <brad.geltz@intel.com>
2019-05-29 11:52:55 -0700

    Add build scripts for "local" GEOPM checkout.
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

---
+++ bin/bin/local_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export PYTHONPATH=/projects/intel/geopm-home/bgeltz/build/geopm_brg/lib/python2.7/site-packages:/projects/intel/geopm-home/shared/lib:/projects/intel/geopm-home/shared/scripts
