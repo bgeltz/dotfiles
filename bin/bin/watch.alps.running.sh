65681324d9e1cfab050c0f49ad110535ec6e7c02
Brad Geltz <brad.geltz@intel.com>
2019-05-29 11:52:02 -0700

    Build and watch scripts tweaked.
    - Do release builds.
    - Sort by score in running.sh.
    - Do the same in the regular watch script.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/watch.alps.running.sh
+++ bin/bin/watch.alps.running.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue:Score --sort State:Queue:JobID --reverse | grep -vE 'hold|queued'
+# qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue:Score --sort State:Queue:JobID --reverse | grep -vE 'hold|queued'
+# qstat --header=JobID:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue:Score --sort State:Queue:JobID --reverse | grep -vE 'hold|queued'
+qstat --header=JobID:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue:Score --sort State:Score:Queue:JobID --reverse | grep -vE 'hold|debug'
--- bin/bin/watch.alps.sh
+++ bin/bin/watch.alps.sh
@@ -4,6 +4,6 @@ QUEUE=Q.GEO_test
 # QUEUE=R.GEO_res1
 
 #watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami) ; echo ; ls -lta ${1}"
-watch -n 3.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue:Score --sort Queue:JobID --reverse -u ${USER} ;"
+watch -n 3.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue:Score --sort State:Score:Queue:JobID --reverse -u ${USER} ;"
 #watch -n 1.0 "qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue -u $(whoami); echo ; qstat --header=JobID:JobName:User:WallTime:QueuedTime:RunTime:TimeRemaining:Nodes:State:Queue ${QUEUE} ;"
 
