c83aef42bdfa2089592449c4048b79e75399a3be
Brad Geltz <brad.geltz@intel.com>
2019-09-26 14:37:42 -0700

    Restrict squeue to current user.
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/watch.slurm.sh
+++ bin/bin/watch.slurm.sh
@@ -2,4 +2,4 @@
 
 # export SINFO_FORMAT="%P %.5a %.10l %.6D %.6t"
 export SQUEUE_FORMAT="%.7i %.8u %.8j %.10M %.12l %.9L %.6D %.8T %.9P %.20S %.20V %r"
-watch -n 1.0 "sinfo; echo ; echo ; squeue"
+watch -n 1.0 "squeue -u ${USER}"
