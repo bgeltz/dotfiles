e909cb591f3574f78fc79cef938270d92d903309
Brad Geltz <brad.geltz@intel.com>
2019-09-27 14:09:30 -0700

    Update submit script for SMNG
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/submit.sh
+++ bin/bin/submit.sh
@@ -10,14 +10,17 @@ fi
 echo "Job ${JOBID} queued at $(date +%F_%H%M.%S)..."
 
 # Wait for job log to appear
-JOB_LOG=${HOME}/output/${JOBID}*.out
+JOB_LOG=${WORK_h1906}/jobs/${JOBID}*.out
 until [ -f ${JOB_LOG} ]; do sleep 2; done
+
+echo "Job ${JOBID} started at $(date +%F_%H%M.%S)..."
 
 # Start a bg tail to watch job output
 tail -f ${JOB_LOG} 2> /dev/null &
 
 # Backup current job script
-OUTPUT_DIR=$(ls -d ${HOME}/output/${JOBID}* | head -n1)
+until [ -d ${WORK_h1906}/output/${JOBID}* ]; do sleep 2; done
+OUTPUT_DIR=$(ls -d ${WORK_h1906}/output/${JOBID}*)
 cp ${1} ${OUTPUT_DIR}
 
 while squeue -u ${USER} | grep ${JOBID} &> /dev/null; do sleep 5; done
@@ -27,7 +30,8 @@ echo "Job ${JOBID} finished at $(date +%F_%H%M.%S)..."
 echo "Job ${JOBID} complete!"
 
 kill %1 2>&1 > /dev/null
-wait > /dev/null 2>&1
+wait
 
 # Backup job output log
-cp ${HOME}/output/${JOBID}*.out ${OUTPUT_DIR}
+cp ${WORK_h1906}/jobs/${JOBID}*.out ~/jobs
+cp ${WORK_h1906}/jobs/${JOBID}*.out ${OUTPUT_DIR}
