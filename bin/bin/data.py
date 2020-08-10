17d155ff9af4988901abd261eafb940a74bde935
Brad Geltz <brad.geltz@intel.com>
2020-08-10 13:41:08 -0700

    Add unique index example
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/data.py
+++ bin/bin/data.py
@@ -8,6 +8,8 @@ data = sys.stdin.read()
 a = pandas.read_csv(StringIO(data), header=None)
 sys.stdout.write('{}\n'.format(a.describe()))
 
+# Get regions
+# a.index.unique(level='region')
 
 #  import code # Doesn't work after a pipe, it's not interactive
 #  code.interact(local=dict(globals(), **locals()), banner="Data in 'a'.")
