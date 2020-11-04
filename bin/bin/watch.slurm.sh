41cc3d1d7d1ee8571a11ea8700fa26e6d10bcf39
Brad Geltz <brad.geltz@intel.com>
2020-11-03 17:43:53 -0800

    Update squeue format
    - Show reason field.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/watch.slurm.sh
+++ bin/bin/watch.slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # export SINFO_FORMAT="%P %.5a %.10l %.6D %.6t"
-export SQUEUE_FORMAT="%.7i %.8u %.8j %.10M %.12l %.9L %.6D %.8T %.9P %.18N %.20S"
+export SQUEUE_FORMAT="%.7i %.8u %.8j %.10M %.12l %.9L %.6D %.8T %.9P %.20S %.20V %r"
 watch -n 1.0 "sinfo; echo ; echo ; squeue"
