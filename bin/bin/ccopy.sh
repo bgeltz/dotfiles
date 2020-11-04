d2e54f97625b0b60221e4a017111faaf68a68d6b
Brad Geltz <brad.geltz@intel.com>
2020-11-03 17:41:14 -0800

    ccopy cleanup
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/ccopy.sh
+++ bin/bin/ccopy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -ex
+
 # HOST=theta
 # HOST=supermucng
 
-set -x
-echo "rsync -avhe ssh --progress ${1}:${2} ."
-# rsync -avhe ssh --progress ${1}:${2} .
-rsync -avhe ssh --progress --exclude '*trace*' ${1}:${2} .
+TARGET=/usr/WS2/intelpwr/intelCRADA/output/${2}
+# TARGET=${2}
+
+# rsync -avhe ssh --progress ${1}:${TARGET} .
+rsync -avhe ssh --progress --exclude '*h5*' --exclude '*trace*' ${1}:${TARGET} .
+
+chmod o+rx $(basename ${2})
+chmod o+r $(basename ${2})/*
+
