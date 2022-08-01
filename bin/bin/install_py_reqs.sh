a22c387da13149c1681b2b94c0106ab82d9c428b
Brad Geltz <brad.geltz@intel.com>
2022-08-01 09:57:18 -0700

    Add integration reqs to installer
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/install_py_reqs.sh
+++ bin/bin/install_py_reqs.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e
-
 if [ -f ${HOME}/.geopmrc ]; then
     source ${HOME}/.geopmrc
 fi
 
-LOG_DIR=${HOME}/public_html/build_logs/$(date +%F_%H)
+LOG_DIR=${HOME}/public_html/build_logs/pip.$(date +%F_%H%M)
 mkdir -p ${LOG_DIR}
 
 rm -fr ${HOME}/.local
@@ -17,6 +15,7 @@ rm -fr ${HOME}/.cache
 python3 -m pip install --user --ignore-installed --upgrade pip setuptools wheel pep517 >> ${LOG_DIR}/pip.log 2>&1 && \
 python3 -m pip install --user --upgrade -r ${GEOPM_SOURCE}/service/requirements.txt >> ${LOG_DIR}/pip.log 2>&1 && \
 python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/scripts/requirements.txt >> ${LOG_DIR}/pip.log 2>&1
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/integration/requirements.txt >> ${LOG_DIR}/pip.log 2>&1
 
 RC=$?
 if [ ${RC} -ne 0 ]; then
