0374841e9f1576c86cf083c21060f546b887e1f6
Brad Geltz <brad.geltz@intel.com>
2022-11-08 17:41:38 -0800

    Add ssh-agent handling
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bash/.bashrc
+++ bash/.bashrc
@@ -122,3 +122,5 @@ export TZ="/usr/share/zoneinfo/US/Pacific"
 
 stty erase '^?'
 stty -ixon
+
+. start_ssh_agent.sh
---
+++ bin/bin/start_ssh_agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://rabexc.org/posts/pitfalls-of-ssh-agents
+
+ssh-add -l &>/dev/null
+if [ "$?" == 2 ]; then
+  test -r ~/.ssh-agent && \
+    eval "$(<~/.ssh-agent)" >/dev/null
+
+  ssh-add -l &>/dev/null
+  if [ "$?" == 2 ]; then
+    (umask 066; ssh-agent > ~/.ssh-agent)
+    eval "$(<~/.ssh-agent)" >/dev/null
+    ssh-add ${GH_KEY}
+  fi
+fi
