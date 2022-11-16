158519c955710b51b96fcecfd63b5981cfbe715a
Brad Geltz <brad.geltz@intel.com>
2022-11-15 16:14:13 -0800

    Updates to install_py_reqs
    - Use realpath relative base to build the URL.
    - Handle env vars being set in parent scripts.
    - Rename refactor.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/install_py_reqs.sh
+++ bin/bin/install_py_reqs.sh
@@ -4,23 +4,32 @@ if [ -f ${HOME}/.geopmrc ]; then
     source ${HOME}/.geopmrc
 fi
 
-LOG_DIR=${HOME}/public_html/build_logs/pip.$(date +%F_%H%M)
-mkdir -p ${LOG_DIR}
+if [ -z "${TIMESTAMP}" ]; then
+    TIMESTAMP=$(date +\%F_\%H\%M)
+fi
+
+if [ -z "${LOG_DIR}" ]; then
+    LOG_DIR=.
+fi
+
+LOG_FILE="${LOG_DIR}/pip.log"
 
 rm -fr ${HOME}/.local
 rm -fr ${HOME}/.cache
 
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py > ${LOG_DIR}/pip.log 2>&1
-# python3 get-pip.py --user > ${LOG_DIR}/pip.log 2>&1
-python3 -m pip install --user --ignore-installed --upgrade pip setuptools wheel pep517 >> ${LOG_DIR}/pip.log 2>&1 && \
-python3 -m pip install --user --upgrade -r ${GEOPM_SOURCE}/service/requirements.txt >> ${LOG_DIR}/pip.log 2>&1 && \
-python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/scripts/requirements.txt >> ${LOG_DIR}/pip.log 2>&1
-python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/integration/requirements.txt >> ${LOG_DIR}/pip.log 2>&1
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py > ${LOG_FILE} 2>&1
+# python3 get-pip.py --user > ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade pip setuptools wheel pep517 >> ${LOG_FILE} 2>&1 && \
+python3 -m pip install --user --upgrade -r ${GEOPM_SOURCE}/service/requirements.txt >> ${LOG_FILE} 2>&1 && \
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/scripts/requirements.txt >> ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/integration/requirements.txt >> ${LOG_FILE} 2>&1
 
 RC=$?
 if [ ${RC} -ne 0 ]; then
-    TEST_LOG="${TEST_OUTPUT_URL}/cron_runs/${TIMESTAMP}/pip.log"
-    ERR_MSG="pip has failed to install the python requirements.  Please see the log for more information:\n${TEST_LOG}\n"
+    SHORT_PATH="$(realpath --relative-base ${HOME}/public_html ${LOG_FILE})"
+
+    LOG_URL="${TEST_OUTPUT_URL}/${SHORT_PATH}"
+    ERR_MSG="pip has failed to install the python requirements.  Please see the log for more information:\n${LOG_URL}\n"
     notify.sh "Integration test failure : ${TIMESTAMP}" "${ERR_MSG}"
     exit 1
 fi
