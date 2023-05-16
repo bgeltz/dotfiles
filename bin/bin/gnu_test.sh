8134628a56a066eb0a5c5a02e2570130dde5fec5
Brad Geltz <brad.geltz@intel.com>
2023-05-16 12:00:38 -0700

    Refactor/fix gnu_test.sh
    - Workaround bash_completion issues on ubuntu.
    
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/gnu_test.sh
+++ bin/bin/gnu_test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export GEOPM_SKIP_COMPILER_CHECK=yes
 source ${HOME}/geopm/integration/config/gnu_env.sh
-GEOPM_SKIP_COMPILER_CHECK=yes source ${HOME}/geopm/integration/config/build_env.sh
-GEOPM_SKIP_COMPILER_CHECK=yes GEOPM_RUN_TESTS=yes GEOPM_GLOBAL_CONFIG_OPTIONS="--enable-debug" ./integration/config/build.sh
+source ${HOME}/geopm/integration/config/build_env.sh
+
+# GEOPM_SKIP_COMPILER_CHECK=yes GEOPM_RUN_TESTS=yes GEOPM_GLOBAL_CONFIG_OPTIONS="--enable-debug" ./integration/config/build.sh
+
+GEOPM_GLOBAL_CONFIG_OPTIONS="--enable-debug" \
+GEOPM_BASE_CONFIG_OPTIONS="--with-bash-completion-dir=${GEOPM_INSTALL}"
+./integration/config/build.sh
