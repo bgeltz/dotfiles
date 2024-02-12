b06153aa81edad82813a25986e5eef49d7ae8155
Brad Geltz <brad.geltz@intel.com>
2024-02-12 09:29:01 -0800

    Add --ignore-installed to install line for service
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- bin/bin/install_py_reqs.sh
+++ bin/bin/install_py_reqs.sh
@@ -20,6 +20,6 @@ rm -fr ${HOME}/.cache
 # curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py > ${LOG_FILE} 2>&1
 # python3 get-pip.py --user > ${LOG_FILE} 2>&1
 python3 -m pip install --user --ignore-installed --upgrade pip setuptools wheel pep517 >> ${LOG_FILE} 2>&1 && \
-python3 -m pip install --user --upgrade -r ${GEOPM_SOURCE}/service/requirements.txt >> ${LOG_FILE} 2>&1 && \
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/service/requirements.txt >> ${LOG_FILE} 2>&1 && \
 python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/scripts/requirements.txt >> ${LOG_FILE} 2>&1
 python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/integration/requirements.txt >> ${LOG_FILE} 2>&1
