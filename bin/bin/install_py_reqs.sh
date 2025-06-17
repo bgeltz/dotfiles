dc3bb10a2842ba633f0df3da0b155578d2e24a6e
Brad Geltz <brad.geltz@intel.com>
2025-06-17 16:12:41 -0700

    Add install_py_reqs.sh
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

---
+++ bin/bin/install_py_reqs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+if [ -f ${HOME}/.geopmrc ]; then
+    source ${HOME}/.geopmrc
+fi
+
+if [ -z "${TIMESTAMP}" ]; then
+    TIMESTAMP=$(date +\%F_\%H\%M)
+fi
+
+if [ -z "${LOG_DIR}" ]; then
+    LOG_DIR=.
+fi
+
+LOG_FILE="${LOG_DIR}/pip.log"
+
+rm -fr ${HOME}/.local
+rm -fr ${HOME}/.cache
+
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py > ${LOG_FILE} 2>&1
+# python3 get-pip.py --user > ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade pip setuptools wheel pep517 >> ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/requirements.txt >> ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/geopmdpy/requirements.txt >> ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/geopmpy/requirements.txt >> ${LOG_FILE} 2>&1
+python3 -m pip install --user --ignore-installed --upgrade -r ${GEOPM_SOURCE}/integration/requirements.txt >> ${LOG_FILE} 2>&1
